58bd9c1dc7ba704363d95dc03aa4c2b6c8e2611f
kayquebandeirasantos <165811456+kayquebandeirasantos@users.noreply.github.com>
2024-11-12 09:56:38 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+perguntas.js: (MUDEM, COLOQUEM AS DE VCS)
+criaCartao(
+'Bíblia',
+'Qual é o livro mais curto da Bíblia?',
+'O livro mais curto da Bíblia é Obadias, que possui apenas 21 versículos.'
+)
+
+criaCartao(
+'Bíblia',
+'Quem escreveu a maioria dos Salmos?',
+'A maioria dos Salmos é atribuída ao rei Davi, embora outros autores também tenham contribuído.'
+)
+
+criaCartao(
+'Bíblia',
+'Qual é o nome do primeiro homem criado segundo a Bíblia?',
+'O primeiro homem criado segundo a Bíblia é Adão.'
+)
+
+criaCartao(
+'Bíblia',
+'Qual é o principal ensinamento do Sermão da Montanha?',
+'O Sermão da Montanha, encontrado em Mateus 5-7, ensina sobre a ética do Reino de Deus, incluindo as bem-aventuranças e a importância do amor e da misericórdia.'
+)

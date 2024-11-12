8ce876d67f1013bf1068e677761820126bfe7811
kayquebandeirasantos <165811456+kayquebandeirasantos@users.noreply.github.com>
2024-11-12 11:39:56 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -6,19 +6,19 @@ criaCartao(
 )
 
 criaCartao(
-'B',
-'Quem escreveu a maioria dos Salmos?',
-'A maioria dos Salmos é atribuída ao rei Davi, embora outros autores também tenham contribuído.'
+'geografia',
+'o rio mais longo?',
+'rio nilo.'
 )
 
 criaCartao(
-'Bíblia',
-'Qual é o nome do primeiro homem criado segundo a Bíblia?',
-'O primeiro homem criado segundo a Bíblia é Adão.'
+'geografia',
+'qual país tem mais população?',
+'china'
 )
 
 criaCartao(
-'Bíblia',
-'Qual é o principal ensinamento do Sermão da Montanha?',
-'O Sermão da Montanha, encontrado em Mateus 5-7, ensina sobre a ética do Reino de Deus, incluindo as bem-aventuranças e a importância do amor e da misericórdia.'
+'geografia',
+'qual é a capital da frança?',
+'paris.'
 )
